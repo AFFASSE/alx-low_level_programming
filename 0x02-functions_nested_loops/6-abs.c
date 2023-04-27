ee87b45ed6cbebb4444c67cb95d7a5b4383b96cc
AFFASSE <nouhailaaffasse99@gmail.com>
2023-04-26 17:17:56 -0700

    abs 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,23 +1,12 @@
 #include "main.h"
 
 /**
-
  * _abs - computes the absolute value of an integrer
-
- *
-
  * @n: the int to check
-
- *
-
  * Return: the absolute value of int
-
- *
-
  */
 
 int _abs(int n)
-
 {
 
 	if (n >= 0)
